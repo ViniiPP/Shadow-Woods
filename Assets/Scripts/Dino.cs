ce55dbb3b0d870bdd4750e63e4bce67f663de31d
Roberto <1135584@atitus.edu.br>
2025-10-26 17:10:16 -0300

    Animação ajutada - tentativa de fazer o chão (Nao funfo)

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -1,60 +1,32 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering.Universal.Internal;
 
 public class Dino : MonoBehaviour
 {
-
-
-    public GroundedCheck groundedCheck;
-
-    Vector2 yVelocity;
-
-    float maxHeight = 1f;
-    float timeToPeak = 0.3f;
-
-    float jumpSpeed;
-    float gravity;
-
-    float groundPosition = 0;
-
-    Rigidbody2D rb;
-
+    [SerializeField]public float jumpForce = 5f;          // Força do pulo
+    public GroundedCheck groundedCheck;   // Script que detecta se está no chão
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        gravity = (2 * maxHeight) / Mathf.Pow(timeToPeak, 2);
-        jumpSpeed = gravity * timeToPeak;
-        groundPosition = transform.position.y;
 
         if (groundedCheck == null)
         {
             Debug.LogError("❌ GroundedCheck não foi atribuído no Inspector no objeto " + gameObject.name);
-            return;
         }
     }
 
     void Update()
     {
-        
-        yVelocity += gravity * Time.deltaTime * Vector2.down;
-
-        if (transform.position.y <= groundPosition && groundedCheck.IsGrounded())
-        {
-            transform.position = new Vector3(transform.position.x, groundPosition, transform.position.z);
-            yVelocity = Vector3.zero;
-        }
-
- 
         if (Input.GetKeyDown(KeyCode.Space) && groundedCheck.IsGrounded())
         {
-            yVelocity = jumpSpeed * Vector2.up;
+    
+            Jump();
         }
-
-
-        //transform.position += (Vector3)(yVelocity * Time.deltaTime);
-        rb.linearVelocity = new Vector2(0, yVelocity.y);
     }
 
-
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
 }
